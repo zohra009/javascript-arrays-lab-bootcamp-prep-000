0278d62f1862c747149fd243f22101610fd60130
anzohra@outlook.com <anzohra@outlook.com>
2018-04-04 21:14:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function prependKitten(name) {
   return kittens
 }
 function removeLastKitten(name) {
-  return kittens.slice(0, kittens.length)
+  return kittens.slice(0, kittens.length - 1)
 }
 
 
